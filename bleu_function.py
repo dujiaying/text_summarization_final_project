0e023a95011d0a72556916f77967175e7e65d5cf
Amy Lin <54963632+xuanamylin@users.noreply.github.com>
2020-12-05 22:31:15 -0600

    Update bleu_function.py

--- bleu_function.py
+++ bleu_function.py
@@ -3,10 +3,8 @@ from nltk.translate.bleu_score import sentence_bleu
 
 def sentence_bleu_n(ref, hyp, weights):
   # ref and hyp are strings
-  # bleu1: weights = [1, 0, 0, 0]
-  # bleu2: weights = [.5, .5, 0, 0]
-  # bleu3: weights = [.33, .33, .33, 0]
-  # bleu3: weights = [.25, .25, .25, .25] <- default in sentence_bleu
+  # the weight list corresponds to [bleu-1_weight, bleu-2_weight, bleu-3_weight, bleu-4_weight]
+  # nltk calculates up to bleu-4
   return sentence_bleu(references = [ref.split()], 
                        hypothesis = hyp.split(),
                        weights = weights)
@@ -15,4 +13,4 @@ def sentence_bleu_n(ref, hyp, weights):
 pred_output['bleu1'] = pred_output[['y_pred', 'y_true']].apply(lambda x: sentence_bleu_n(x[1], x[0], weights = [1,0,0,0]), axis=1)
 
 # bleu2
-pred_output['bleu2'] = pred_output[['y_pred', 'y_true']].apply(lambda x: sentence_bleu_n(x[1], x[0], weights = [.5,.5,0,0]), axis=1)
+pred_output['bleu2'] = pred_output[['y_pred', 'y_true']].apply(lambda x: sentence_bleu_n(x[1], x[0], weights = [0,1,0,0]), axis=1)
